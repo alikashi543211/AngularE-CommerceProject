471380a0ef8b71a29c3a16a8ce925dddeffa8eb8
kashifalidevstudio <kashifali@devstudio.us>
2023-05-26 13:33:38 +0500

    create interface and use it in product service

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -1,5 +1,7 @@
+import { Product } from './product';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,34 +10,35 @@ export class ProductService {
 
     constructor(private _httpClient:HttpClient) { }
 
-    createProduct(productBody:any)
+    createProduct(productBody:any):Observable<Product>
     {
         const baseUrl = "http://localhost:3000/products";
-        return this._httpClient.post(baseUrl, productBody);
+        return this._httpClient.post<Product>(baseUrl, productBody);
     }
-    viewProduct(productId:any)
+    viewProduct(productId:any):Observable<Product>
     {
         const baseUrl = "http://localhost:3000/product/" + productId;
-        return this._httpClient.get(baseUrl);
+        return this._httpClient.get<Product>(baseUrl);
     }
-    updateProduct(productId:any, productBody:any)
+    updateProduct(productId:any, productBody:any):Observable<Product>
     {
         const baseUrl = "http://localhost:3000/product/" + productId;
-        return this._httpClient.put(baseUrl, productBody);
+        return this._httpClient.put<Product>(baseUrl, productBody);
     }
-    deleteProduct(productId:any)
+    deleteProduct(productId:any):Observable<Product>
     {
         const baseUrl = "http://localhost:3000/product/" + productId;
-        return this._httpClient.delete(baseUrl, productId);
+        return this._httpClient.delete<Product>(baseUrl);
     }
-    searchCategoryProduct(categoryID:any)
+    searchCategoryProduct(categoryID:any):Observable<Product>
     {
         const baseUrl = "http://localhost:3000/product/category=" + categoryID;
-        return this._httpClient.get(baseUrl);
+        return this._httpClient.get<Product>(baseUrl);
     }
-    searchDateProduct(dateParam:any)
+    searchDateProduct(dateParam:any):Observable<Product>
     {
         const baseUrl = "http://localhost:3000/product/date=" + dateParam;
-        return this._httpClient.get(baseUrl);
+        return this._httpClient.get<Product>(baseUrl);
     }
+
 }
--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,2 +1,12 @@
 export interface Product {
+    productId:string;
+    categoryId:number;
+    productName:string;
+    description:string;
+    rating:string;
+    price:number;
+    productImg:string;
+    isAvailabile:boolean;
+    color:string;
+    review:number;
 }
